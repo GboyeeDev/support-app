5c7a24834d6780097fd658e0c6d7e5c91a5f57a7
GbpyeeDev <olugboyega.ojelabi@gmail.com>
2023-06-11 19:25:18 +0100

    Added controllers and database

---
+++ backend/routes/users/usersController.js
@@ -0,0 +1,78 @@
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// REGISTER A NEW USER
+// @ URL: /api/users
+// access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
+  // To find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exists');
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // creat user by registering
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// LOG IN A USERS
+// @ URL: /api/users/login
+// access Public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // check if user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+// Generate tokens
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('server.....');
+const express = require('express');
+const colors = require('colors');
+const app = express();
+require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 5000;
+
+//Database Connection
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/users/usersRoute'));
+
+// Middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
