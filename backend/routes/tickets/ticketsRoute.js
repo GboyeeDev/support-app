fb49fa97d8f53576961e8889409f958bb8409de8
GbpyeeDev <olugboyega.ojelabi@gmail.com>
2023-08-02 21:59:21 +0100

    Created Update and Delete features for tickets

--- backend/routes/tickets/ticketsRoute.js
+++ backend/routes/tickets/ticketsRoute.js
@@ -4,12 +4,14 @@ const {
   getTickets,
   getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../tickets/ticketsController');
 
 const { protect } = require('../../middleware/authMiddleware');
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
 
-router.route('/:id').get(protect, getTicket);
+router.route('/:id').get(protect, getTicket).delete(protect, deleteTicket).put(protect, updateTicket);
 
 module.exports = router;
